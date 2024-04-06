1437d1aa0d23c230043b5d8034a8109d5337bd1b
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-04-06 05:47:48 +0100

    Fix caching logic in FIFO and LIFO cache implementations

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -19,7 +19,7 @@ class FIFOCache(BaseCaching):
 
             if key in self.cache_data:
                 self.cache_data[key] = item
-                # return
+
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 item_to_discard = self.key_list.pop(0)
                 del self.cache_data[item_to_discard]
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 """2-2-lifo_cache module"""
 
-BaseCaching = __import__('base_caching.py').BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
     """LIFOCache class definition"""
 
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.lifo_key_list = []
 
     def put(self, key, item):
         """set item to cache data"""
         if key and item:
             self.cache_data[key] = item
-            return
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                item_to_discard = self.lifo_key_list.pop(-1)
+                del self.cache_data[item_to_discard]
+                print("DISCARD:", item_to_discard)
+
+            self.cache_data[key] = item
+            self.lifo_key_list.append(key)
+
+    def get(self, key):
+        """get item from cache"""
+        if key in self.cache_data:
+            return self.cache_data[key]
